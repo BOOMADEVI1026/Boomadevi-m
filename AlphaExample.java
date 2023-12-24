4ddcaf2e5c87f8ba964dbe2d6393dccf8b30d841
BOOMADEVI <152686407+BOOMADEVI1026@users.noreply.github.com>
2023-12-24 10:51:11 +0530

    Add files via upload

---
+++ AlphaExample.java
@@ -0,0 +1,12 @@
+
+public class AlphaExample {
+    public static void main(String [] arg)
+    {
+        char firstchar='A';
+        char lastchar='Z';
+        for(char startchar=firstchar;startchar<=lastchar;startchar++){
+        System.out.println(startchar+"");
+    }
+    }
+    
+}
